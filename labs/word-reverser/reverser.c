eab7f5982b92d21b21dac2c59cd3394bafbb9805
Alan Gonzalez <argaalan@hotmail.com>
2020-03-03 18:18:25 -0600

    word-reverser

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
-int main(){
-    // Place your magic here
+int main()
+
+{
+    int c;
+    int words[100];
+    int pos = 0;
+    while ((c = getchar()) != EOF)
+    {
+
+        if (c == '\n')
+        {
+            //Comenzar a dar reversa a las palabras
+            if (pos != 0)
+            {
+                for (int i = pos - 1; i >= 0; i--)
+                {
+                    printf("%c", words[i]);
+                }
+                printf("\n");
+                pos = 0;
+            }
+        }
+        else
+        {
+            words[pos] = c;
+            pos++;
+        }
+    }
     return 0;
 }
