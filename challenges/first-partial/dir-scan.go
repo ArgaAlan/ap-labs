a754ed06eaeeb26a960d4adb65eb24aa7625fc83
Alan Gonzalez <argaalan@hotmail.com>
2020-03-24 13:55:10 -0600

    Solve first partial
    Signed-off-by: Alan Gonzalez <argaalan@hotmail.com>

--- challenges/first-partial/dir-scan.go
+++ challenges/first-partial/dir-scan.go
@@ -3,19 +3,60 @@ package main
 import (
 	"fmt"
 	"os"
+        "path/filepath"
 )
+
+func walkFn(root string,info os.FileInfo, e error) error{
+    if e != nil{ //Si el error no es diferente de null hubo un problema con el path y se debe eliminar este walk
+        fmt.Printf("WRONG PATH")
+        return e
+    }
+    
+    switch mode := info.Mode(); true{
+	case mode.IsDir():
+		dir++
+	case mode&os.ModeSymlink != 0:
+		sysL++
+	case mode&os.ModeDevice != 0:
+		dev++
+	case mode&os.ModeSocket != 0:
+		sock++
+	default:
+		others++
+	}
+	
+    return nil
+}
 
 // scanDir stands for the directory scanning implementation
 func scanDir(dir string) error {
-	return nil
+	e := filepath.Walk(dir, walkFn)
+        if e != nil {
+            return e
+        }
+        return nil
 }
-
+var dir, sysL, dev, sock, others int
 func main() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: ./dir-scan <directory>")
 		os.Exit(1)
 	}
+        e := scanDir(os.Args[1]);
+	if e!=nil {
+          fmt.Println(e);
+          os.Exit(1);
+        }
 
-	scanDir(os.Args[1])
+        fmt.Println("Directory Scanning Tool")
+	fmt.Println("+-----------------+----------+")
+	fmt.Printf("| Path            | %-9s|\n", os.Args[1])
+	fmt.Println("+-----------------+----------+")
+	fmt.Printf("| Directories     | %-9d|\n", dir)
+	fmt.Printf("| Symbolic Links  | %-9d|\n", sysL)
+	fmt.Printf("| Devices         | %-9d|\n", dev)
+	fmt.Printf("| Sockets         | %-9d|\n", sock)
+	fmt.Printf("| Other files     | %-9d|\n", others)
+	fmt.Println("+-----------------+----------+")
 }
--- challenges/first-partial/pacman-analyzer.c
+++ challenges/first-partial/pacman-analyzer.c
@@ -1,25 +1,230 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #define REPORT_FILE "packages_report.txt"
 
-void analizeLog(char *logFile, char *report);
+struct Program
+{
+    char *name;
+    char *install;
+    char *update;
+    int nUpdates;
+    char *removal;
+};
 
-int main(int argc, char **argv) {
+struct Program programs[1000];
+int totalPrograms, numPrograms, upPrograms, remPrograms;
 
-    if (argc < 2) {
-	printf("Usage:./pacman-analizer.o pacman.log\n");
-	return 1;
+char *concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + 2);
+    strcpy(result, s1);
+    strcat(result, " ");
+    strcat(result, s2);
+    return result;
+}
+
+char* readLine(int rd){
+    int size = 50;
+    char *line;
+    line = calloc(size, sizeof(char));
+    int acum = 0;
+    int n;
+    while((n = read(rd, line+acum, size)) > 0){
+        for(int i=acum;i<acum+n;i++){
+            if(line[i] == '\n'){
+                lseek(rd, i-(acum+n)+1, SEEK_CUR);
+                line[i] = '\0';
+                return line;
+            }
+        }
+        acum+=n;
+        line = realloc(line, acum+size);
+    }
+    if(acum>0){
+        line[acum] = '\0';
+        return line;
+    }
+    return NULL;
+}
+
+char **linetoSentence(char *string) //La linea que lei la paso a un array de palabras
+{
+    char **words = calloc(20, sizeof(char *)); //Array de strings de tamaño 20 y 30 letras por palabra
+    for (int i = 0; i < 20; i++)
+    {
+        words[i] = calloc(100, sizeof(char));
+    }
+    int j = 0;
+    int word = 0;
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (string[i] == ' ')
+        {
+            words[word][j] = '\0';
+            word++;
+            j = 0;
+        }
+        else if (string[i] == '\0')
+        {
+            return words;
+        }
+        else
+        {
+            if (string[i] != '[' && string[i] != ']')
+            { //Elimino los corchetes para poder manejar mejor la fecha
+                words[word][j] = string[i];
+                j++;
+            }
+        }
+    }
+    printf("\n");
+    return words;
+}
+
+void createReport(int idW)
+{
+    char number[10];
+    int x;
+    write(idW, "Pacman Packages Report\n", 23);
+    write(idW, "----------------------\n", 23);
+    write(idW, "- Installed packages : ", 23);
+    x = sprintf(number, "%d\n", totalPrograms);
+    write(idW, number, x);
+    write(idW, "- Removed packages   : ", 23);
+    x = sprintf(number, "%d\n", remPrograms);
+    write(idW, number, x);
+    write(idW, "- Upgraded packages  : ", 23);
+    x = sprintf(number, "%d\n", upPrograms);
+    write(idW, number, x);
+    write(idW, "- Current installed  : ", 23);
+    x = sprintf(number, "%d\n", numPrograms);
+    write(idW, number, x);
+
+    write(idW, "\nList of packages\n----------------\n", 35);
+    for (int i = 0; i < totalPrograms; i++)
+    {
+        write(idW, "- Package Name        : ", 24);
+        write(idW, programs[i].name, strlen(programs[i].name));
+        write(idW, "\n  - Install date      : ", 25);
+        write(idW, programs[i].install, strlen(programs[i].install));
+        write(idW, "\n  - Last update date  : ", 25);
+        if (programs[i].update != NULL)
+        {
+            write(idW, programs[i].update, strlen(programs[i].update));
+        }
+        else
+        {
+            write(idW, "-", 1);
+        }
+        write(idW, "\n  - How many updates  : ", 25);
+        x = sprintf(number, "%d\n", programs[i].nUpdates);
+        write(idW, number, x);
+        write(idW, "  - Removal date      : ", 24);
+        if (programs[i].removal != NULL)
+        {
+            write(idW, programs[i].removal, strlen(programs[i].removal));
+        }
+        else
+        {
+            write(idW, "-", 1);
+        }
+        write(idW, "\n", 1);
+    }
+    close(idW);
+}
+
+void analizeLog(char *logFile, char *report)
+{
+    printf("Generating Report from: [%s] log file\n", logFile);
+    int idR = open(logFile, O_RDONLY);
+    if (idR < 0)
+    {
+        printf("Input file not found.\n");
+        return;
+    }
+    char *line;
+    char **words;
+    while ((line = readLine(idR)) != NULL)
+    {
+        words = linetoSentence(line);
+        if (strcmp("installed", words[3]) == 0)
+        {
+            programs[totalPrograms].name = words[4];
+            programs[totalPrograms].install = concat(words[0], words[1]);
+            totalPrograms++;
+            numPrograms++;
+        }
+        else if (strcmp("upgraded", words[3]) == 0)
+        {
+            char *prog = words[4];
+            int notComplete = 1;
+            for (int i = 0; i < totalPrograms; i++)
+            {
+                if (strcmp(prog, programs[i].name) == 0)
+                {
+                    programs[i].update = concat(words[0], words[1]);
+                    programs[i].nUpdates++;
+                    upPrograms++;
+                    notComplete = 1;
+                }
+            }
+            if (notComplete)
+            {
+                printf("Program not found, can't be upgraded.\n");
+            }
+	    free(prog);
+        }
+        else if (strcmp("removed", words[3]) == 0)
+        {
+            char *prog = words[4];
+            int notComplete = 1;
+            for (int i = 0; i < totalPrograms; i++)
+            {
+                if (strcmp(prog, programs[i].name) == 0)
+                {
+                    programs[i].removal = concat(words[0], words[1]);
+                    numPrograms--;
+                    remPrograms++;
+                    notComplete = 1;
+                }
+            }
+            if (notComplete)
+            {
+                printf("Program not found, can't be removed.\n");
+            }
+            free(prog);
+        }
+        free(line);
+        free(words);
     }
 
-    analizeLog(argv[1], REPORT_FILE);
+    close(idR);
+    //Comienza la escritura del reporte
+    int idW = open(report, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (idW < 0)
+    {
+        printf("Output file not found.\n");
+        return;
+    }
 
+    createReport(idW);
+    close(idW);
+    printf("Report is generated at: [%s]\n", report);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage:./pacman-analizer.o pacman.log\n");
+        return 1;
+    }
+    analizeLog(argv[1], REPORT_FILE);
     return 0;
 }
 
-void analizeLog(char *logFile, char *report) {
-    printf("Generating Report from: [%s] log file\n", logFile);
-
-    // Implement your solution here.
-
-    printf("Report is generated at: [%s]\n", report);
-}
